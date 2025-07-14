de40791add1c7733a6509cb0a6da147e2326162f
Behrooz Zarebavani <behrooz@nia-login02.scinet.local>
2025-07-14 14:23:23 -0400

    build script is updated

--- EvaluationPipeline/build_ipc.sh
+++ EvaluationPipeline/build_ipc.sh
@@ -5,16 +5,18 @@ set -euo pipefail            # good practice: fail fast & echo unset vars
 
 #--- Bind mounts -------------------------------------------------------------
 # Apptainer’s binding path
-export APPTAINER_BIND="Address/To/IPC/Cloned/Repository:/mnt"   # :contentReference[oaicite:0]{index=0}
+# export APPTAINER_BIND="Address/To/IPC/Cloned/Repository:/mnt"   # :contentReference[oaicite:0]{index=0}
+export APPTAINER_BIND="$SCRATCH/test/parth-ipc-benchmark-generator:/mnt"
+
 
 #--- Container image ---------------------------------------------------------
 # Apptainer doesn’t care what you call this variable, but for clarity we
 # rename it.  If you prefer the old name simply keep using $SING_SIF.
-APP_SIF="${APP_SIF:-/path/to/your_image.sif}"
+APP_SIF="${APP_SIF:-$SCRATCH/test/parth-ipc-benchmark-generator/EvaluationPipeline/parth_docker.sif}"
 
 #--- Build inside the container ---------------------------------------------
 apptainer exec "${APP_SIF}" bash -c '
-    cd /mnt/IPC-dev2            && \
+    cd /mnt/                    && \
     mkdir -p build              && \
     cd build                    && \
     rm -f CMakeCache.txt        && \
